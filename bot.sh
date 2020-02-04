4f4bec4c81393e589b698cff1e43652a208db34f
James Conrad <james.conrad@uoit.net>
2020-02-04 17:06:34 -0500

    fix parent script not doing a hard reset

--- bot.sh
+++ bot.sh
@@ -4,9 +4,10 @@
 while true; do
     node index.js &>>log
     val=$?
-    if [$val -eq 0]; then
+    if [ $val -eq 0 ]; then
         echo "Pulling from Github."
-        git pull origin/master
+        git fetch
+        git reset --hard origin/master
         echo "Pull complete, Restarting."
     else
         echo "Program exited unsuccessfully, Restarting."
