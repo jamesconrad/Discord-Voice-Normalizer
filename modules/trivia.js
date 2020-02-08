a7c8d98bdf51434cf1fe2da2a729560f448859f0
James Conrad <james.conrad@uoit.net>
2020-02-08 18:29:54 -0500

    fixed html entity decoding
    removed unnecessary check in disqualification checks, fixed typo and missing parameter relating to  refreshtoken, added comments

--- modules/trivia.js
+++ modules/trivia.js
@@ -7,6 +7,11 @@ const command = require('../modules/command');
 
 const config = require('../config.json');
 
+//decode strings, decoder taken from: https://github.com/mdevils/node-html-entities/blob/master/lib/html4-entities.js npm package: html-entities
+let HTML_ALPHA = ['apos', 'nbsp', 'iexcl', 'cent', 'pound', 'curren', 'yen', 'brvbar', 'sect', 'uml', 'copy', 'ordf', 'laquo', 'not', 'shy', 'reg', 'macr', 'deg', 'plusmn', 'sup2', 'sup3', 'acute', 'micro', 'para', 'middot', 'cedil', 'sup1', 'ordm', 'raquo', 'frac14', 'frac12', 'frac34', 'iquest', 'Agrave', 'Aacute', 'Acirc', 'Atilde', 'Auml', 'Aring', 'Aelig', 'Ccedil', 'Egrave', 'Eacute', 'Ecirc', 'Euml', 'Igrave', 'Iacute', 'Icirc', 'Iuml', 'ETH', 'Ntilde', 'Ograve', 'Oacute', 'Ocirc', 'Otilde', 'Ouml', 'times', 'Oslash', 'Ugrave', 'Uacute', 'Ucirc', 'Uuml', 'Yacute', 'THORN', 'szlig', 'agrave', 'aacute', 'acirc', 'atilde', 'auml', 'aring', 'aelig', 'ccedil', 'egrave', 'eacute', 'ecirc', 'euml', 'igrave', 'iacute', 'icirc', 'iuml', 'eth', 'ntilde', 'ograve', 'oacute', 'ocirc', 'otilde', 'ouml', 'divide', 'oslash', 'ugrave', 'uacute', 'ucirc', 'uuml', 'yacute', 'thorn', 'yuml', 'quot', 'amp', 'lt', 'gt', 'OElig', 'oelig', 'Scaron', 'scaron', 'Yuml', 'circ', 'tilde', 'ensp', 'emsp', 'thinsp', 'zwnj', 'zwj', 'lrm', 'rlm', 'ndash', 'mdash', 'lsquo', 'rsquo', 'sbquo', 'ldquo', 'rdquo', 'bdquo', 'dagger', 'Dagger', 'permil', 'lsaquo', 'rsaquo', 'euro', 'fnof', 'Alpha', 'Beta', 'Gamma', 'Delta', 'Epsilon', 'Zeta', 'Eta', 'Theta', 'Iota', 'Kappa', 'Lambda', 'Mu', 'Nu', 'Xi', 'Omicron', 'Pi', 'Rho', 'Sigma', 'Tau', 'Upsilon', 'Phi', 'Chi', 'Psi', 'Omega', 'alpha', 'beta', 'gamma', 'delta', 'epsilon', 'zeta', 'eta', 'theta', 'iota', 'kappa', 'lambda', 'mu', 'nu', 'xi', 'omicron', 'pi', 'rho', 'sigmaf', 'sigma', 'tau', 'upsilon', 'phi', 'chi', 'psi', 'omega', 'thetasym', 'upsih', 'piv', 'bull', 'hellip', 'prime', 'Prime', 'oline', 'frasl', 'weierp', 'image', 'real', 'trade', 'alefsym', 'larr', 'uarr', 'rarr', 'darr', 'harr', 'crarr', 'lArr', 'uArr', 'rArr', 'dArr', 'hArr', 'forall', 'part', 'exist', 'empty', 'nabla', 'isin', 'notin', 'ni', 'prod', 'sum', 'minus', 'lowast', 'radic', 'prop', 'infin', 'ang', 'and', 'or', 'cap', 'cup', 'int', 'there4', 'sim', 'cong', 'asymp', 'ne', 'equiv', 'le', 'ge', 'sub', 'sup', 'nsub', 'sube', 'supe', 'oplus', 'otimes', 'perp', 'sdot', 'lceil', 'rceil', 'lfloor', 'rfloor', 'lang', 'rang', 'loz', 'spades', 'clubs', 'hearts', 'diams'];
+let HTML_CODES = [39, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255, 34, 38, 60, 62, 338, 339, 352, 353, 376, 710, 732, 8194, 8195, 8201, 8204, 8205, 8206, 8207, 8211, 8212, 8216, 8217, 8218, 8220, 8221, 8222, 8224, 8225, 8240, 8249, 8250, 8364, 402, 913, 914, 915, 916, 917, 918, 919, 920, 921, 922, 923, 924, 925, 926, 927, 928, 929, 931, 932, 933, 934, 935, 936, 937, 945, 946, 947, 948, 949, 950, 951, 952, 953, 954, 955, 956, 957, 958, 959, 960, 961, 962, 963, 964, 965, 966, 967, 968, 969, 977, 978, 982, 8226, 8230, 8242, 8243, 8254, 8260, 8472, 8465, 8476, 8482, 8501, 8592, 8593, 8594, 8595, 8596, 8629, 8656, 8657, 8658, 8659, 8660, 8704, 8706, 8707, 8709, 8711, 8712, 8713, 8715, 8719, 8721, 8722, 8727, 8730, 8733, 8734, 8736, 8743, 8744, 8745, 8746, 8747, 8756, 8764, 8773, 8776, 8800, 8801, 8804, 8805, 8834, 8835, 8836, 8838, 8839, 8853, 8855, 8869, 8901, 8968, 8969, 8970, 8971, 9001, 9002, 9674, 9824, 9827, 9829, 9830];
+let alphaIndex = {};
+
 let trivia = {
     apiTokens: new Map(),//session token
     categories: [],
@@ -36,12 +41,23 @@ async function Initialize() {
     }).on("error", (err) => {
         console.log("Trivia HTTP Error: " + err.message);
     });
+    //build decode indexer
+    let i = 0;
+    let length = HTML_ALPHA.length;
+    while (i < length) {
+        let alpha = HTML_ALPHA[i];
+        let code = HTML_CODES[i];
+        alphaIndex[alpha] = String.fromCharCode(code);
+        i++;
+    }
     console.log('Trivia Initialized.');
 }
 exports.Initialize = Initialize;
 
 async function Trivia(message, args) {
+    //store current time for activity checks
     trivia.lastUsed = new Date();
+    //fetch a trivia token if this guild dosn't have one
     if (!trivia.apiTokens.has(message.guild.id))
         await GenerateNewTriviaToken(message.guild.id);
     let token = trivia.apiTokens.get(message.guild.id);
@@ -49,12 +65,14 @@ async function Trivia(message, args) {
     let apicall = `https://opentdb.com/api.php?amount=1&token=${token}`;
     let repeatIdx = 0;
     let repeatCount = 0;
+    //parse args
     for (i = 0; i < args.length; i++) {
+        //list categories
         if (args[i] == '-categories') {
             let retString = 'Possible categories are:';
             trivia.categories.forEach(c => (retString += `\n${c.name}, ID: ${c.id}`));
             return message.channel.send(retString);
-        } else if (args[i] == '-c') {
+        } else if (args[i] == '-c') { //check for specified category
             if (i >= args.length - 1) return message.channel.send('You need to specifiy a category id after the -c argument, list the categories and their ids with -categories');
             let id = Number(args[i + 1]);
             if (id === NaN) return message.channel.send(`${id} is not a number, and therefore an invalid category`);
@@ -65,31 +83,39 @@ async function Trivia(message, args) {
                     break;
                 }
             }
+            //if not valid, return an error response, if valid carry on with parsing
             if (!validId) return message.channel.send(`${id} is not a valid category id number, double check categories with -categories`);
             apicall += `&category=${id}`;
-        } else if (args[i] == '-score') {
+        } else if (args[i] == '-score') { //check for listing scores
             let retString = `\`\`\`${message.guild.name} Trivia Scores:`;
+            //pull trivia players from guild_members table
             let sql = `SELECT user_id id FROM guild_members WHERE guild_id = ${message.guild.id}`;
             database.all(sql, async (row) => {
                 let table = [];
+                //no row means user no players in the guild
                 if (!row) return message.channel.send(`Nobody on this server has played trivia.`);
+                //wait for each user to be fetched from users
                 await Promise.all(row.map(async (r) => {
                     let entry = await database.getPromise(`SELECT user_id id, name n, score s FROM users WHERE user_id = ${r.id}`, async (userEntry) => {
                         if (!userEntry) return;
                         return userEntry;
                     });
+                    //add user to table
                     table.push(entry);
                 }));
+                //we have all users, time to sort then form and send a response
                 table = table.sort((a,b) => (b.s - a.s));
                 table.forEach(e => retString += `\n${unescape(e.n)}: ${e.s}`);
                 return message.channel.send(retString + '\`\`\`');
             });
+            //exit function, response will come once sql queries are complete
             return;
-        } else if (args[i] == '-reset') {
-            await RefreshTriviaToken();
-        } else if (args[i] == '-h' || args[i] == '-help') {
+        } else if (args[i] == '-reset') { //check for token refresh
+            await RefreshTriviaToken(message.guild.id);
+        } else if (args[i] == '-h' || args[i] == '-help') { //depreciated, users should always use !help for any command help
             return message.channel.send('Use !help for a comprehensive list of commands.');
-        } else if (args[i] == '-r' && args.length > i + 1){
+        } else if (args[i] == '-r' && args.length > i + 1) { //repeat mode
+            //store number of repeats remaining
             let n = Number(args[i+1]);
             if (n === NaN)
                 return message.channel.send('Invalid repeat count.');
@@ -97,14 +123,16 @@ async function Trivia(message, args) {
             repeatIdx = i+1;
         }
     }
-
+    //fetch the trivia question
     https.get(apicall, async (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;
         });
         resp.on('end', async() => {
-            let question = JSON.parse(data);
+            //decode the html entities out
+            let question = JSON.parse(DecodeAmpersandHTML(data));
+            //handle response codes
             if (question.response_code == 2) message.channel.send(`Trivia API Error: ${apicall} returned invalid parameter`)
             else if (question.response_code == 4) {
                 await RefreshTriviaToken(message.guild.id);
@@ -113,58 +141,78 @@ async function Trivia(message, args) {
                 await GenerateNewTriviaToken(message.guild.id);
                 return Trivia(message, args);
             }
+            //fetch the first (only) question
             question = question.results[0];
-            question.question = unescape(question.question);
             let questionReward = TriviaDifficultyToScore(question.difficulty);
             
             let correct_answer;
             let formattedResponse = '';
             let answerEmotes;
             if (question.type == 'multiple') {
+                //form and randomize awnser array order
                 let answerIdx = 0;
                 let answers = [];
                 answerEmotes = [`🇦`, `🇧`, `🇨`, `🇩`]
                 answers = question.incorrect_answers.sort(() => Math.random() - 0.5);
                 answerIdx = Math.floor(Math.random() * answers.length);
                 answers.splice(answerIdx, 0, question.correct_answer);
-                if (repeatCount >= 0) formattedResponse += `Questions remaing in repeat: ${repeatCount}`;
+                //if on repeating mode, give feedback
+                if (repeatCount > 0) formattedResponse += `Questions remaing in repeat: ${repeatCount}`;
+                //build the response
                 formattedResponse += `\`\`\`Category: ${question.category}\nDifficulty: ${question.difficulty}\`\`\`\n**${question.question}**\n`
                 for (i = 0; i < answers.length; i++)
                     formattedResponse += `\n${answerEmotes[i]} - ${answers[i]}`
                 correct_answer = answerEmotes[answerIdx];
             } else {
+                //form the response
                 formattedResponse = `\`\`\`Category: ${question.category}\nDifficulty: ${question.difficulty}\`\`\`\n**${question.question}**\n`
                 correct_answer = question.correct_answer === 'True' ? '✅' : '❎';
                 answerEmotes = [`✅`, `❎`];
             }
+            //send the response
             message.channel.send(formattedResponse).then(m => {
+                //add the reacts
                 answerEmotes.forEach(e => m.react(e));
 
                 const filter = (reaction, user) => (/*reaction.emoji.name == correct_answer && !user.bot*/true);
+                //fetch reacts after the timeout has expired
                 m.awaitReactions(filter, { time: trivia.timeout }).then(collected => {
                     let correct_users = '';
                     let disqual_users = '';
+                    //fetch all correct users
                     let finalWinners = collected.get(correct_answer).users;
+                    //check for disqualified users (submitted more than 1 answer)
                     let disqualified = [];
-                    for(i = 0; i < answerEmotes.length; i++) {
+                    for (i = 0; i < answerEmotes.length; i++) {
+                        //skip checking correct answer
                         if (answerEmotes[i] == correct_answer) continue;
+                        //check on each user that were and still are correct
                         finalWinners = finalWinners.filter(u => {
+                            //pull the user from this incorrect answer's reacts
                             let vs = collected.get(answerEmotes[i]).users.get(u.id);
+                            //confirm this is still a valid user
                             let a = finalWinners.get(u.id);
-                            if (vs && a && a.bot == false && vs.id == a.id)
+                            //check if user reacted to incorrect answer && user is valid && user is not a bot
+                            if (vs && a && a.bot == false)
+                                //add user to disqualified array if they are not already in it
                                 if (!disqualified.includes(vs.username))
-                                disqualified.push(vs.username);
-                            return !(vs && a && vs.id == a.id);
+                                    disqualified.push(vs.username);
+                            //return filter, remove this user from the finalWinners if they are disqualified
+                            return !(vs && a);
                         });
                     }
+                    //build response string
                     finalWinners.forEach(user => correct_users += ` ${user.username},`);
                     disqualified.forEach(u => disqual_users += ` ${u},`);
                     ModTriviaScores(finalWinners, questionReward, message.guild);
                     
+                    //remove trailing commas
                     correct_users = correct_users.slice(0, -1);
                     disqual_users = disqual_users.slice(0, -1);
+                    //build final response
                     let retString = `Answer: ${correct_answer}`;
                     if (disqualified.length > 0) retString += `\nDisqualified: ${disqual_users}`;
+                    //send final response, if in repeat mode, also re-call this function
                     if (repeatCount > 0) {
                         m.channel.send(retString + (correct_users == '' ? `\nYou all suck.` : `\nCongrats:` + correct_users));
                         args[repeatIdx] = repeatCount - 1;
@@ -207,7 +255,7 @@ function TriviaDifficultyToScore(difficulty) {
 }
 exports.TriviaDifficultyToScore = TriviaDifficultyToScore;
 
-function RefreshTriviaToken(guidId) {
+function RefreshTriviaToken(guildId) {
     return new Promise(resolve => {
         https.get(`https://opentdb.com/api_token.php?command=reset&token=${trivia.apiTokens.get(guildId)}`, (resp) => {
             let data = '';
@@ -266,3 +314,24 @@ function AddHelpPages() {
     };
     help.AddPage('trivia', page);
 }
+
+function DecodeAmpersandHTML(str) {
+    if (!str || !str.length) {
+        return '';
+    }
+    return str.replace(/&(#?[\w\d]+);?/g, function(s, entity) {
+        var chr;
+        if (entity.charAt(0) === "#") {
+            var code = entity.charAt(1).toLowerCase() === 'x' ?
+                parseInt(entity.substr(2), 16) :
+                parseInt(entity.substr(1));
+
+            if (!(isNaN(code) || code < -32768 || code > 65535)) {
+                chr = String.fromCharCode(code);
+            }
+        } else {
+            chr = alphaIndex[entity];
+        }
+        return chr || s;
+    });
+};
